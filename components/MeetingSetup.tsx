adc5a49280759814c470a81287933cecca9ec4f3
Yann-Donald <yanndonald237@gmail.com>
2024-10-25 18:49:29 +0100

    fourth commit, meeting setup

---
+++ components/MeetingSetup.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import {
+  DeviceSettings,
+  useCall,
+  VideoPreview,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value: boolean) => void;
+}) => {
+  const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
+  const call = useCall();
+
+  if (!call) {
+    throw new Error("useCall must be used within a streamCall component");
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
+      <h1 className="text-2xl font-bold">Setup</h1>
+      <VideoPreview />
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            checked={isMicCamToggledOn}
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+            className="appearance-none w-6 h-6 border border-gray-300 rounded-md checked:border-transparent focus:outline-none focus:ring-blue-500
+           checked:bg-blue-500 checked:text-white"
+          />
+          Join with Mic and Camera off
+        </label>
+        <DeviceSettings />
+      </div>
+      <Button
+        className="rounded-md bg-teal-500 px-4 py-2 text-black hover:bg-teal-700 hover:text-white"
+        onClick={() => {
+          call.join();
+          setIsSetupComplete(true);
+        }}
+      >
+        Join Meeting
+      </Button>
+    </div>
+  );
+};
+
+export default MeetingSetup;
